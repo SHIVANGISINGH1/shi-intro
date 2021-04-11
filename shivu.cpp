5490f98fed862fdd1631ba76ab7372469a9bbf41
shivangisingh1 <shivangisinghhappy@gmail.com>
2021-04-11 23:18:29 +0530

    hii

---
+++ shivu.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+ 
+
+int main()
+ 
+{
+    int main(){
+
+int i,j,r;
+cin>>r;
+for(i=1;i<=r;i++)
+{
+int ;
+for(j=1;j<=(r-i);j++)
+{
+cout<<" ";
+`
+for( ;j<=r;j++)
+{
+cout<<"*"<<" ";
+`
+for( ;j<(r+i);j++)
+{
+cout<<"*"<<" ";
+`
+for( ;j<=(2*r-1);j++)
+{
+cout<<" ";
+`
+
+cout<<endl;
+`
+    return 0;
+}
+
+    
+                  
+
